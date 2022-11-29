78cf3a1492fce43cf73f49e74a903bd1eb83d85e
insight-jtoledo <john.toledo@insight.com>
2022-11-29 09:59:51 +0800

    deploy using Deneb's configuration

--- Deployment script.ps1
+++ Deployment script.ps1
@@ -1,1 +1,6 @@
-﻿New-AzManagementGroupDeployment -Name CustomerOnboardviaPolicy -Location AustraliaEast -ManagementGroupId 4005dec5-fb4a-48a2-9fd8-b22d223db569 -TemplateFile "C:\Users\akumar3\OneDrive - Insight\Desktop\Lighthouse templates\Lighthouse Policy\deployLighthouseIfNotExistManagementGroup.json" -TemplateParameterFile "C:\Users\akumar3\OneDrive - Insight\Desktop\Lighthouse templates\Lighthouse Policy\deployLighthouseIfNotExistsManagementGroup.parameters.json" -verbose
+﻿New-AzManagementGroupDeployment -Name CustomerOnboardviaPolicy `
+-Location AustraliaEast -ManagementGroupId 4005dec5-fb4a-48a2-9fd8-b22d223db569 `
+-TemplateFile .\deployLighthouseIfNotExistManagementGroup.json `
+-TemplateParameterFile .\deployLighthouseIfNotExistsManagementGroup.parameters.json `
+-WhatIf `
+-Confirm:$false -Verbose
