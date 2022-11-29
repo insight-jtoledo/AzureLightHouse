72b9630d9ebf24c9293d864bc925e2e02f523cc4
insight-jtoledo <john.toledo@insight.com>
2022-11-29 13:27:18 +0800

    deploynew

--- Deploymentscript.ps1
+++ Deploymentscript.ps1
@@ -1,6 +1,6 @@
 ﻿New-AzManagementGroupDeployment -Name CustomerOnboardviaPolicy `
 -Location AustraliaEast -ManagementGroupId 4005dec5-fb4a-48a2-9fd8-b22d223db569 `
--TemplateFile .\deployLighthouseIfNotExistManagementGroup.json `
--TemplateParameterFile .\deployLighthouseIfNotExistsManagementGroup.parameters.json `
+-TemplateFile .\deployLighthousepolicy.json `
+-TemplateParameterFile .\deployLighthousepolicy.parameters.json `
 -WhatIf `
 -Confirm:$false -Verbose
