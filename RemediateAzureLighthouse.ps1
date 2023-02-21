c8731a2d0d25158932469e8b49d885cf58e00a78
insight-jtoledo <118139313+insight-jtoledo@users.noreply.github.com>
2023-02-21 11:01:47 +0800

    Create RemediateAzureLighthouse.ps1

---
+++ RemediateAzureLighthouse.ps1
@@ -0,0 +1,23 @@
+param(
+$TenantID,
+$ManagementGroupName,
+$Location,
+$PolicyDefinitionName="Enable-Azure-Lighthouse"
+)
+
+# Start Remediation Task
+Write-Host ""
+Write-Host "Getting all non-compliant resources.."
+$NonCompliantPolicies = Get-AzPolicyState `
+| Where-Object{$_.ComplianceState -eq 'NonCompliant' `
+-and $_.PolicyDefinitionAction -eq 'deployifnotexists' `
+-and $_.PolicyAssignmentName -eq $PolicyDefinitionName.Name}
+
+Write-Host ""
+Write-Host "Remediating existing resources that are not compliant.."
+$nonCompliantPolicies | ForEach-Object{
+    $name = ("rem." + $_.PolicyDefinitionName)
+    Start-AzPolicyRemediation -Name $name `
+    -PolicyAssignmentId $_.PolicyAssignmentId `
+    -Scope $managementGroup.Id
+    }
