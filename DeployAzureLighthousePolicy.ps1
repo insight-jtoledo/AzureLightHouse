e9b53909d22f21fbd015cfcd5831008ea298f1f4
insight-jtoledo <118139313+insight-jtoledo@users.noreply.github.com>
2023-02-22 19:07:43 +0800

    Update DeployAzureLighthousePolicy.ps1

--- DeployAzureLighthousePolicy.ps1
+++ DeployAzureLighthousePolicy.ps1
@@ -1,103 +1,68 @@
 param(
-$PolicyDefinitionName="Enable-Azure-Lighthouse"
+[parameter(Mandatory)][string]$ManagementGroupName,
+[parameter(Mandatory)][string]$Location,
+[parameter(Mandatory)][string]$PolicyDefinitionName
 )
 
-# Get Details
-$ManagementGroupName = Read-Host "Enter Management Group Name"
-$Location = Read-Host "Enter Location"
-$ResourceGroupName = Read-Host "Enter Azure Guardian Resource Group Name"
-Write-Host ""
-
-# Get Management Group Details
+#Get Management Group Details
 $ManagementGroup = Get-AzManagementGroup | Where-Object{$_.DisplayName -eq $ManagementGroupName}
+if($ManagementGroup -eq $null){
+    Write-Host "Please check Management Group Name" -ForegroundColor Yellow
+    }else{Write-Host "Verified Management Group Name - $ManagementGroupName" -ForegroundColor Cyan}
 
-# Deploy Policy
-Write-Host ""
-Write-Host "Deploying Azure Lighthouse Policy.." -ForegroundColor Cyan
-<#New-AzManagementGroupDeployment -Name $PolicyDefinitionName -Location $Location `
--ManagementGroupId $ManagementGroup.Name `
--TemplateFile '.\deployLighthouseIfNotExistManagementGroup.json' `
--TemplateParameterFile '.\deployLighthouseIfNotExistsManagementGroup.parameters.json' -verbose
-#>
+#Check if Policy Definition Name exists
+$PolicyDefinitionName = Get-AzPolicyDefinition | where-object{$_.Name -like $PolicyDefinitionName}
+if($PolicyDefinitionName -eq $null){
+    }else{
+    #Deploy Policy
+    Write-Host "Deploying Azure Lighthouse Policy.." -ForegroundColor Cyan
+    New-AzManagementGroupDeployment -Name $PolicyDefinitionName -Location $Location `
+    -ManagementGroupId $ManagementGroup.Name `
+    -TemplateFile '.\deployLighthouseIfNotExistManagementGroup.json' `
+    -TemplateParameterFile '.\deployLighthouseIfNotExistsManagementGroup.parameters.json' -verbose
+    Write-Host "Policy Definition - $PolicyDefinitionName already exist. Please specify a new one.-" -ForegroundColor Yellow
+    }
+
 # Get the policy assignment from the management group
-$PolicyDefinition = Get-AzPolicyDefinition | where-object{$_.Name -like $PolicyDefinitionName}
+$PolicyDefinitionName = Get-AzPolicyDefinition | where-object{$_.Name -like $PolicyDefinitionName}
 
 # Create a new policy assignment with the policy definition
-Write-Host ""
 Write-Host "Assigning policy to the specified Management Group.." -ForegroundColor Cyan
-#New-AzPolicyAssignment -Name $PolicyDefinition.ResourceName -Scope $ManagementGroup.Id -PolicyDefinition $PolicyDefinition -IdentityType SystemAssigned -Location australiaeast
-$PolicyAssignment = Get-AzPolicyAssignment -PolicyDefinitionId $PolicyDefinition.PolicyDefinitionId
-$RoleDefinitionId = [GUID]($PolicyDefinition.properties.policyRule.then.details.roleDefinitionIds -split "/")[4]
+New-AzPolicyAssignment -Name $PolicyDefinitionName.ResourceName `
+-Scope $ManagementGroup.Id `
+-PolicyDefinition $PolicyDefinitionName `
+-IdentityType SystemAssigned `
+-Location australiaeast
+
+$PolicyAssignment = Get-AzPolicyAssignment -PolicyDefinitionId $PolicyDefinitionName.PolicyDefinitionId
+
+$RoleDefinitionId = [GUID]($PolicyDefinitionName.properties.policyRule.then.details.roleDefinitionIds -split "/")[4]
+
 $ObjectID = [GUID]($PolicyAssignment.Identity.principalId)
+
 Start-Sleep 90
-#New-AzRoleAssignment -Scope $managementGroup.Id -ObjectId $ObjectID -RoleDefinitionId $RoleDefinitionId
 
-Write-Host "Policy for Azure Lighthouse has been completed" -ForegroundColor Cyan
+New-AzRoleAssignment -Scope $managementGroup.Id -ObjectId $ObjectID -RoleDefinitionId $RoleDefinitionId
+
+$Remediate = Read-Host "Remediate Non Compliant Resources? (Y/N)"
+
+if($Remediate='Y'){
+    
+# Start Remediation Task
 Write-Host ""
-Write-Host "Deploying Azure Lighthouse to $ResourceGroupName" -ForegroundColor Cyan
+Write-Host "Getting all non-compliant resources.."
+$NonCompliantPolicies = Get-AzPolicyState `
+| Where-Object{$_.ComplianceState -eq 'NonCompliant' `
+-and $_.PolicyDefinitionAction -eq 'deployifnotexists' `
+-and $_.PolicyAssignmentName -eq $PolicyDefinitionName.Name}
 
-$deploymentOptions = @{}
-Write-Host "Getting Azure subscriptions (filtering out unsupported ones)..." -ForegroundColor Green
-
-$subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" -and $_.SubscriptionPolicies.QuotaId -notlike "Internal*" -and $_.SubscriptionPolicies.QuotaId -notlike "AAD*" }
-
-if ($subscriptions.Count -gt 1) {
-
-    $selectedSubscription = -1
-    for ($i = 0; $i -lt $subscriptions.Count; $i++)
-    {
-        if (-not($deploymentOptions["SubscriptionId"]))
-        {
-            Write-Output "[$i] $($subscriptions[$i].Name)"    
-        }
-        else
-        {
-            if ($subscriptions[$i].Id -eq $deploymentOptions["SubscriptionId"])
-            {
-                $selectedSubscription = $i
-                break
-            }
-        }
+Write-Host ""
+Write-Host "Remediating existing resources that are not compliant.."
+$nonCompliantPolicies | ForEach-Object{
+    $name = ("rem." + $_.PolicyDefinitionName)
+    Start-AzPolicyRemediation -Name $name `
+    -PolicyAssignmentId $_.PolicyAssignmentId `
+    -Scope $managementGroup.Id
     }
-    if (-not($deploymentOptions["SubscriptionId"]))
-    {
-        $lastSubscriptionIndex = $subscriptions.Count - 1
-        while ($selectedSubscription -lt 0 -or $selectedSubscription -gt $lastSubscriptionIndex) {
-            Write-Output "---"
-            $selectedSubscription = [int] (Read-Host "Please, select the target subscription for this deployment [0..$lastSubscriptionIndex]")
-        }    
-    }
-    if ($selectedSubscription -eq -1)
-    {
-        throw "The selected subscription does not exist. Check if you are logged in with the right Azure AD account."        
-    }
-}
-else
-{
-    if ($subscriptions.Count -ne 0)
-    {
-        $selectedSubscription = 0
-    }
-    else
-    {
-        throw "No valid subscriptions found. Azure AD or Internal subscriptions are currently not supported."
-    }
-}
 
-if ($subscriptions.Count -eq 0) {
-    throw "No subscriptions found. Check if you are logged in with the right Azure AD account."
-}
-
-$subscriptionId = $subscriptions[$selectedSubscription].Id
-
-if (-not($deploymentOptions["SubscriptionId"]))
-{
-    $deploymentOptions["SubscriptionId"] = $subscriptionId
-}
-
-if ($ctx.Subscription.Id -ne $subscriptionId) {
-    $ctx = Select-AzSubscription -SubscriptionId $subscriptionId
-}
-
-# Deploying Azure Lighthouse delegation for Azure Guardian Resource Group
-New-AzSubscriptionDeployment -Name "RGDeployment" -Location $Location -TemplateFile .\resourcegroup.template.json -rgName $ResourceGroupName
+    }else{Write-Host "Deployment Done" -ForegroundColor Cyan}
