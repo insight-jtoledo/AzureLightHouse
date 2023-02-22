da64ac6ee0f02d2786da359688b2f3aef9d64dc3
insight-jtoledo <118139313+insight-jtoledo@users.noreply.github.com>
2023-02-22 19:18:51 +0800

    Update DeployAzureLighthousePolicy.ps1

--- DeployAzureLighthousePolicy.ps1
+++ DeployAzureLighthousePolicy.ps1
@@ -1,7 +1,8 @@
 param(
 [parameter(Mandatory)][string]$ManagementGroupName,
 [parameter(Mandatory)][string]$Location,
-[parameter(Mandatory)][string]$PolicyDefinitionName
+[parameter(Mandatory)][string]$PolicyDefinitionName,
+[parameter(Mandatory)][string]$ResourceGroupName
 )
 
 #Get Management Group Details
@@ -66,3 +67,71 @@ $nonCompliantPolicies | ForEach-Object{
     }
 
     }else{Write-Host "Deployment Done" -ForegroundColor Cyan}
+
+Write-Host "Deploying Azure Lighthouse to $ResourceGroupName" -ForegroundColor Cyan
+
+$deploymentOptions = @{}
+Write-Host "Getting Azure subscriptions (filtering out unsupported ones)..." -ForegroundColor Green
+
+$subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" -and $_.SubscriptionPolicies.QuotaId -notlike "Internal*" -and $_.SubscriptionPolicies.QuotaId -notlike "AAD*" }
+
+if ($subscriptions.Count -gt 1) {
+
+    $selectedSubscription = -1
+    for ($i = 0; $i -lt $subscriptions.Count; $i++)
+    {
+        if (-not($deploymentOptions["SubscriptionId"]))
+        {
+            Write-Output "[$i] $($subscriptions[$i].Name)"    
+        }
+        else
+        {
+            if ($subscriptions[$i].Id -eq $deploymentOptions["SubscriptionId"])
+            {
+                $selectedSubscription = $i
+                break
+            }
+        }
+    }
+    if (-not($deploymentOptions["SubscriptionId"]))
+    {
+        $lastSubscriptionIndex = $subscriptions.Count - 1
+        while ($selectedSubscription -lt 0 -or $selectedSubscription -gt $lastSubscriptionIndex) {
+            Write-Output "---"
+            $selectedSubscription = [int] (Read-Host "Please, select the target subscription for this deployment [0..$lastSubscriptionIndex]")
+        }    
+    }
+    if ($selectedSubscription -eq -1)
+    {
+        throw "The selected subscription does not exist. Check if you are logged in with the right Azure AD account."        
+    }
+}
+else
+{
+    if ($subscriptions.Count -ne 0)
+    {
+        $selectedSubscription = 0
+    }
+    else
+    {
+        throw "No valid subscriptions found. Azure AD or Internal subscriptions are currently not supported."
+    }
+}
+
+if ($subscriptions.Count -eq 0) {
+    throw "No subscriptions found. Check if you are logged in with the right Azure AD account."
+}
+
+$subscriptionId = $subscriptions[$selectedSubscription].Id
+
+if (-not($deploymentOptions["SubscriptionId"]))
+{
+    $deploymentOptions["SubscriptionId"] = $subscriptionId
+}
+
+if ($ctx.Subscription.Id -ne $subscriptionId) {
+    $ctx = Select-AzSubscription -SubscriptionId $subscriptionId
+}
+
+# Deploying Azure Lighthouse delegation for Azure Guardian Resource Group
+New-AzSubscriptionDeployment -Name "RGDeployment" -Location $Location -TemplateFile .\resourcegroup.template.json -rgName $ResourceGroupName
